818b2cdce628369feddb52663776788bbe3e4204
Zachary Yaro <zmyaro@zmyaro.com>
2017-04-24 19:49:12 -0400

    Add STAR API key param

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
+var API_KEY = 'FILL_THIS_IN'; // Do not push the API key to the repo!
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	var xhr = new XMLHttpRequest();
 	
-	xhr.open('GET', 'http://ritstar.com/api/member?id=' + request.memberId, true);
+	xhr.open('GET', 'http://ritstar.com/api/member?id=' + request.memberId + '&key=' + API_KEY, true);
 	
 	xhr.onreadystatechange = function () {
 		if (xhr.readyState === 4) {
