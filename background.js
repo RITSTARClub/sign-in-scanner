eb487cce75d702258dc4a3bdcbb4bdb965b429c4
Zachary Yaro <zmyaro@zmyaro.com>
2017-05-13 23:36:01 -0400

    Fix error in updated regex

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var API_KEY = 'FILL_THIS_IN', // Do not push the API key to the repo!
-	FORM_URL_REGEX = '^https://docs\.google\.com/forms/.+/viewform(\?.*)?$';
+	FORM_URL_REGEX = '^https://docs\.google\.com/forms/.+/viewform([?].*)?$';
 
 // Set the rule when the extension is installed or upgraded.
 chrome.runtime.onInstalled.addListener(function() {
